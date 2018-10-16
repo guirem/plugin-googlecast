e8af66a79a1724e1a3b6cb3ccb40dac8d71d5db7
guirem@gmail.com <guirem@gmail.com>
2018-10-16 21:02:24 +0200

    adding auto reinstall for pip in case of failure

--- resources/install.sh
+++ resources/install.sh
@@ -26,6 +26,10 @@ pip3cmd=$(compgen -ac | grep -E '^pip-?3' | sort -r | head -1)
 if [[ -z  $pip3cmd ]]; then     # pip3 not found
     if python3 -m pip -V 2>&1 | grep -q -i "^pip " ; then     # but try other way
         pip3cmd="python3 -m pip"
+    else # something is wrong with pip3 so reinstall it
+        echo "-- Something is wrong with pip3, trying to re-install :"
+        sudo apt-get -y --reinstall install python3-pip
+        pip3cmd=$(compgen -ac | grep -E '^pip-?3' | sort -r | head -1)
     fi
 fi
 if [[ ! -z  $pip3cmd ]]; then     # pip3 found
