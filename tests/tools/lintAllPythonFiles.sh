78fa03d505a4e36e059df14bc6a8ecb93f6e5249
rem_libizi <remi@libizi.fr>
2020-01-29 15:08:05 +0100

    pylint cleaning

--- tests/tools/lintAllPythonFiles.sh
+++ tests/tools/lintAllPythonFiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for file in `find . -name "*.py" ! -name "__init__.py"`;
+for file in `find ./resources -name "*.py" ! -name "__init__.py"`;
   do
       echo "Check $file with pylint"
-      python3 -m pylint --rcfile=tests/tools/.pylintrc $file
+      python3 -m flake8 $file
   done
